96cfa52802e0e204d4a8c6a082950634aa2635c9
SAYED JUNED IQUBAL <junediqubal95@gmail.com>
2025-10-07 07:21:46 +0530

    Delete api directory

--- api/src/utils/redisClient.js
+++
@@ -1,7 +0,0 @@
-import Redis from "ioredis";
-
-const redisURL = process.env.REDIS_URL || "redis://redis:6379";
-export const redis = new Redis(redisURL);
-
-redis.on("connect", () => console.log("✅ Redis connected"));
-redis.on("error", (err) => console.error("❌ Redis error:", err));
