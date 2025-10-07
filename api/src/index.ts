06f89c8a3be5f3051a824061e23b94681abec342
SAYED JUNED IQUBAL <junediqubal95@gmail.com>
2025-10-07 09:23:15 +0530

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,11 @@
 import Fastify from 'fastify';
 import health from './routes/health';
+import jobs from './routes/jobs';
 
 const server = Fastify();
 
 server.register(health);
+server.register(jobs);
 
 server.listen({ port: 8080 }, (err, address) => {
   if (err) throw err;
