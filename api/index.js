1b06372116e1aa1acb43e62cc464d167f536c492
SAYED JUNED IQUBAL <junediqubal95@gmail.com>
2025-10-07 21:00:39 +0530

    Delete api directory

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/api/health", (req, res) => {
-  res.json({ status: "ok" });
-});
-
-// Export default for Vercel
-module.exports = app;
